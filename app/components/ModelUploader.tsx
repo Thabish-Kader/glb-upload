7fbc3c23ea6d45269154d94e97155ba572a93c12
Thabish <kadertabish2@gmail.com>
2023-07-03 15:07:14 +0530

    destructure-model
    Signed-off-by: Thabish <kadertabish2@gmail.com>

---
+++ app/components/ModelUploader.tsx
@@ -0,0 +1,54 @@
+"use client";
+import { OrbitControls, useGLTF } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { useState } from "react";
+
+interface ModelProps {
+	url: string;
+}
+
+function Model({ url }: ModelProps) {
+	const { nodes } = useGLTF(url);
+	return (
+		<>
+			{nodes &&
+				Object.values(nodes).map((node, index) => (
+					<mesh
+						key={index}
+						geometry={node.geometry}
+						material={node.material}
+					/>
+				))}
+		</>
+	);
+}
+
+function ModelUploader() {
+	const [url, seturl] = useState("./product.glb");
+	const handleFileUpload = async (
+		event: React.ChangeEvent<HTMLInputElement>
+	) => {
+		const file = event.target.files?.[0];
+		if (file) {
+			const reader = new FileReader();
+			reader.onload = (e) => {
+				const url = e.target?.result as string;
+				seturl(url);
+			};
+			reader.readAsDataURL(file);
+		}
+	};
+
+	return (
+		<div className="h-screen">
+			<input type="file" accept=".glb" onChange={handleFileUpload} />
+			<Canvas>
+				<ambientLight />
+				<OrbitControls />
+				<Model url={url} />
+			</Canvas>
+		</div>
+	);
+}
+
+export default ModelUploader;
